ddc3966ad902189dff05e87cf54aae40291ef20c
Samuel_ssekasi <samuelsekasi201@gmail.com>
2026-07-10 15:23:45 +0300

    Refactor student registration to use dictionaries

--- python-secure-student-registration.py
+++ python-secure-student-registration.py
@@ -1,17 +1,17 @@
-name={}
-marks={}
-age={}
-i=0
+students=[]
+
 while True:
- name[i]=input("Enter student's name: ")
- if name[i].lower()=="exit":
+    
+ student={}  
+ student["name"]=input("Enter student's name: ")
+ if student["name"].lower()=="exit":
     break
  while True:
     try:
-     age[i]=int(input("Enter student's age: "))
-     if age[i]>0:
+     student["age"]=int(input("Enter student's age: "))
+     if student["age"]>0:
         break
-     elif age[i]<160:
+     elif student["age"]<160:
         break
      else :
         print("Invalid age try agin!")
@@ -20,16 +20,16 @@ while True:
 
  while True:
    try:
-     marks[i]=int(input("Enter student's marks: "))
-     if marks[i]>0 and marks[i]<100:
+     student["marks"]=int(input("Enter student's marks: "))
+     if student["marks"]>0 and student["marks"]<100:
         break
      else :
         print("Invalid marks try agin!")
    except ValueError:
         print("Invalid marks try agin!")    
  print("Enter exit to stop: ")
- i+=1
+ students.append(student)
 print()
 print(f"{"="*5}STUDENT RECORDS{"="*5}")
-for j in range(i):
-  print(f"{name[j]}--{age[j]}yrs old-- scored{marks[j]}%")
+for student in students:
+  print(f"{student["name"]}--{student["age" ]}yrs old-- scored{student["marks"]}%")
